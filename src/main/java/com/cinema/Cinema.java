22f53ddb28d472556eb42e19b9bb6daed0d64636
ignaciogarcia <igarcia1@mail.austral.edu.ar>
2024-09-12 20:08:33 -0300

    hecho

--- src/main/java/com/cinema/Cinema.java
+++ src/main/java/com/cinema/Cinema.java
@@ -2,27 +2,19 @@ package com.cinema;
 
 import java.util.ArrayList;
 
-/**
- * Clase que representa una sala de cine.
- */
+
 public class Cinema {
+
 
     private Seat[][] seats;
 
-    /**
-     * Construye una sala de cine. Se le pasa como dato un arreglo cuyo tamaño
-     * es la cantidad de filas y los enteros que tiene son el número de butacas en cada fila.
-     */
+
     public Cinema(int[] rows) {
         seats = new Seat[rows.length][];
         initSeats(rows);
     }
 
-    /**
-     * Inicializa las butacas de la sala de cine.
-     *
-     * @param rows arreglo que contiene la cantidad de butacas en cada fila
-     */
+
     private void initSeats(int[] rows) {
         for (int i = 0; i < rows.length; i++) {
             seats[i] = new Seat[rows[i]];
@@ -34,25 +26,45 @@ public class Cinema {
         }
     }
 
-    /**
-     * Cuenta la cantidad de seats disponibles en el cine.
-     */
+
     public int countAvailableSeats() {
-        ...
+        int count = 0;
+        for (int i = 0; i < seats.length; i++){
+            for (int j = 0; j < seats[i].length;j++){
+                if (seats[i][j].isAvailable()){
+                    count+= 1;
+                }
+            }
+        }
+        return count;
     }
 
-    /**
-     * Busca la primera butaca libre dentro de una fila o null si no encuentra.
-     */
+
     public Seat findFirstAvailableSeatInRow(int row) {
-        ...
+        if (row < 0 || row >= seats.length) {
+            return null;
+
+        };
+        for (int j = 0; j < seats[row].length; j++){
+            if (seats[row][j].isAvailable()){
+                return seats[row][j];
+            }
+
+        }
+        return null;
+
     }
 
-    /**
-     * Busca la primera butaca libre o null si no encuentra.
-     */
+
     public Seat findFirstAvailableSeat() {
-        ...
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[i].length; j++) {
+                if (seats[i][j].isAvailable()){
+                    return seats[i][j];
+                }
+            }
+        }
+        return null;
     }
 
     /**
@@ -63,7 +75,22 @@ public class Cinema {
      * @return La primer butaca de la serie de N butacas, si no hay retorna null.
      */
     public Seat getAvailableSeatsInRow(int row, int amount) {
-        ...
+        int count = 0;
+        if(amount <= 0 || amount > seats[row].length){
+           return null;
+        };
+        for (int i = 0; i < seats[row].length; i++) {
+            if (seats[row][i].isAvailable()){
+                count += 1;
+            }
+            else{
+                count = 0;
+            }
+            if (count == amount){
+                return seats[row][i - amount + 1];
+            }
+        }
+        return null;
     }
 
     /**
@@ -73,7 +100,24 @@ public class Cinema {
      * @param amount el número de butacas pedidas.
      */
     public Seat getAvailableSeats(int amount) {
-        ...
+       int count = 0;
+       if (amount < 0 || amount > seats.length){
+           return null;
+       }
+       for (int i = 0; i < seats.length; i++) {
+           for (int j = 0; j < seats[i].length; j++) {
+               if (seats[i][j].isAvailable()) {
+                   count += 1;
+               }
+               else{
+                   count = 0;
+               }
+               if (count == amount) {
+                   return seats[i][j - amount + 1];
+               }
+            }
+        }
+        return null;
     }
 
     /**
@@ -83,7 +127,18 @@ public class Cinema {
      * @param amount la cantidad de butacas a reservar.
      */
     public void takeSeats(Seat seat, int amount) {
-        ...
+        int row = seat.getRow();
+        int number = seat.getSeatNumber();
+
+        if (seats[row].length >= number + amount) {
+            for (int i = number; i < (number + amount); i++) {
+                seats[row][i].takeSeat();
+            }
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("Not enough seats");
+        }
+
     }
 
     /**
@@ -93,6 +148,19 @@ public class Cinema {
      * @param amount la cantidad de butacas a liberar.
      */
     public void releaseSeats(Seat seat, int amount) {
-        ...
+        int row = seat.getRow();
+        int number = seat.getSeatNumber();
+
+        if (seats[row].length >= number + amount) {
+            for (int i = number; i < (number + amount); i++) {
+                seats[row][i].releaseSeat();
+            }
+        }
+        else {
+            throw new ArrayIndexOutOfBoundsException("Not enough seats");
+        }
+
     }
+
+
 }
--- src/main/java/com/cinema/Seat.java
+++ src/main/java/com/cinema/Seat.java
@@ -1,5 +1,7 @@
 package com.cinema;
 
+import java.util.List;
+
 /**
  * Clase que representa una butaca en el cine. Posee una fila y un lugar en la fila.
  * Se puede saber si está ocupada o no.
@@ -10,35 +12,34 @@ public class Seat {
     private int seatNumber;
     private boolean available = true;
 
-    /** Construye butacas, para ello se le debe pasar su ubicación, representada por
-     * la fila y el número de asiento. */
     public Seat(int row, int seatNumber) {
-        ...
+        this.row = row;
+        this.seatNumber = seatNumber;
     }
 
     /** Retorna el número de la fila. */
     public int getRow() {
-        ...
+        return this.row;
     }
 
     /** Retorna el número de asiento dentro de la fila. */
     public int getSeatNumber() {
-        ...
+        return this.seatNumber;
     }
 
     /** Retorna true si el asiento está disponible, o false en caso contrario. */
     public boolean isAvailable() {
-        ...
+        return this.available;
     }
 
     /** Ocupa el asiento. */
     public void takeSeat() {
-        ...
+        this.available = false;
     }
 
     /** Desocupa el asiento. */
     public void releaseSeat() {
-        ...
+        this.available = true;
     }
 }
 
